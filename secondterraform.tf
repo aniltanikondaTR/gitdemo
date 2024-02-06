de18d507e1fe89f879ae082f2c181069b467d8d0
Anil.Tanikonda <anilt0712@gmail.com>
2024-02-06 18:59:20 +0530

    i am committing my first and second terraform codes

---
+++ secondterraform.tf
@@ -0,0 +1,3 @@
+This is my second terraform code file.
+
+I am started to learn the git and gitm from udemy.
---
+++ terraform.tf
@@ -0,0 +1,2 @@
+This is my first terraform code file.
+I want to learn the DevOps tools to became a devops engineer
